3df9e1f420d3abf9e5350432e85e037511d89357
DanZanoria <71779837+DanZanoria@users.noreply.github.com>
2021-02-08 22:01:37 -0500

    edit erros final

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_pymongo import PyMongo
 from sqlalchemy import create_engine
 from flask import Flask, render_template, redirect
 from scrape_mars import Marsmon
+import scrape_mars
+
 
 #################################################
 # Flask Setup
@@ -18,7 +20,7 @@ def home():
     Marsmon = mongo.db.Marsmon.find_one()
     return render_template("index.html")
 
-@app.route("/scrape")
+@mdamon.route("/scrape")
 def scrape():
     Marsmon = mongo.db.Marsmon
     FMars = scrape_mars.scrap()
